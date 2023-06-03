3ac92b1d9e819f8e5682572bac662845be07f59a
Dikman Satya Nugraha <dikmansn@gmail.com>
2023-06-03 22:35:28 +0700

    edit add.php

--- add.php
+++ add.php
@@ -1,51 +1,56 @@
-
 <html>
+
 <head>
-    <title>Add Users</title>
+    <title>Tambah User</title>
 </head>
- 
+
 <body>
-    <a href="index.php">Go to Home</a>
-    <br/><br/>
- 
+    <a href="index.php">Kembali ke Home</a>
+    <br /><br />
+
     <form action="add.php" method="post" name="form1">
         <table width="25%" border="0">
-            <tr> 
-                <td>Name</td>
-                <td><input type="text" name="name"></td>
+            <tr>
+                <td>ID</td>
+                <td><input type="text" name="ID"></td>
             </tr>
-            <tr> 
-                <td>Email</td>
-                <td><input type="text" name="email"></td>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="Nama"></td>
             </tr>
-            <tr> 
-                <td>Mobile</td>
-                <td><input type="text" name="mobile"></td>
+            <tr>
+                <td>Kode Mata Kuliah</td>
+                <td><input type="text" name="Kode Mata Kuliah"></td>
             </tr>
-            <tr> 
+            <tr>
+                <td>Deskripsi</td>
+                <td><input type="text" name="Deskripsi"></td>
+            </tr>
+            <tr>
                 <td></td>
                 <td><input type="submit" name="Submit" value="Add"></td>
             </tr>
         </table>
     </form>
-    
+
     <?php
- 
+
     // Check If form submitted, insert form data into users table.
-    if(isset($_POST['Submit'])) {
+    if (isset($_POST['Submit'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $mobile = $_POST['mobile'];
-        
+
         // include database connection file
         include_once("config.php");
-                
+
         // Insert user data into table
         $result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
-        
+
         // Show message when user added
         echo "User added successfully. <a href='index.php'>View Users</a>";
     }
     ?>
 </body>
+
 </html>
