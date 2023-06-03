d3e358f76571cb5cbc639ab793ddb71e2872f757
Dikman Satya Nugraha <dikmansn@gmail.com>
2023-06-03 22:51:44 +0700

    edit add.php

--- add.php
+++ add.php
@@ -37,18 +37,19 @@
 
     // Check If form submitted, insert form data into users table.
     if (isset($_POST['Submit'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $mobile = $_POST['mobile'];
+        $id = $_POST['id'];
+        $nama = $_POST['nama'];
+        $kode_matakuliah = $_POST['kode_matakuliah'];
+        $deskripsi = $_POST['deskripsi'];
 
         // include database connection file
         include_once("config.php");
 
         // Insert user data into table
-        $result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
+        $result = mysqli_query($mysqli, "INSERT INTO matakuliah(id,nama,kode_matakuliah,deskripsi) VALUES('$id','$nama','$kode_matakuliah','$deskripsi')");
 
         // Show message when user added
-        echo "User added successfully. <a href='index.php'>View Users</a>";
+        echo "Mata Kuliah berhasil ditambahkan. <a href='index.php'>Lihat Mata Kuliah</a>";
     }
     ?>
 </body>
