26f26dbeb17379e2a46434e2dbd137ab4bc352e8
Dikman Satya Nugraha <dikmansn@gmail.com>
2023-06-03 22:44:06 +0700

    edit index.php

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <?php
 // Create database connection using config file
 include_once("config.php");
- 
+
 // Fetch all users data from database
-$result = mysqli_query($mysqli, "SELECT * FROM dosen ORDER BY id DESC");
+$result = mysqli_query($mysqli, "SELECT * FROM matakuliah ORDER BY id DESC");
 ?>
- 
+
 <html>
-<head>    
-    <title>Homepage</title>
+
+<head>
+    <title>Mata Kuliah</title>
 </head>
- 
+
 <body>
-<a href="add.php">Add New User</a><br/><br/>
- 
+    <a href="add.php">Tambah Mata Kuliah</a><br /><br />
+
     <table width='80%' border=1>
- 
-    <tr>
-        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
-    </tr>
-    <?php  
-    while($user_data = mysqli_fetch_array($result)) {         
-        echo "<tr>";
-        echo "<td>".$user_data['name']."</td>";
-        echo "<td>".$user_data['mobile']."</td>";
-        echo "<td>".$user_data['email']."</td>";    
-        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
-    }
-    ?>
+
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Kode Mata Kuliah</th>
+            <th>Deskripsi</th>
+            <th>Update</th>
+        </tr>
+        <?php
+        while ($user_data = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $user_data['id'] . "</td>";
+            echo "<td>" . $user_data['nama'] . "</td>";
+            echo "<td>" . $user_data['matakuliah'] . "</td>";
+            echo "<td>" . $user_data['deskripsi'] . "</td>";
+            echo "<td><a href='edit.php?id=$user_data[id]'>Ubah</a> | <a href='delete.php?id=$user_data[id]'>Hapus</a></td></tr>";
+        }
+        ?>
     </table>
 </body>
+
 </html>
